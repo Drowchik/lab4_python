fbce1be24126b25cf1b13b60cf9ad463b407e236
natal <natal@CLOWN>
2023-11-20 22:20:53 +0400

    done task 1-4

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pandas as pd
 
 
-df = pd.read_csv(r"C:\Users\natal\lab_2_python\annotation1.csv", sep=',')
-
 if __name__ == '__main__':
-    print(df.head(9))
+    path = r"C:\Users\natal\lab_2_python\annotation1.csv"
+    df_csv = pd.read_csv(path)
+
+    texts = []
+    for absolute_path, rating in zip(df_csv['absolute_path'], df_csv['rating']):
+        with open(absolute_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+            texts.append((text, rating))
+
+    df = pd.DataFrame(texts, columns=['Рецензия', 'Оценка'])
+
+    cas = df.isnull().sum()
+    print(cas)
+    df.dropna()
+    print('------')
+    df['CountWord'] = df['Рецензия'].apply(lambda word: len(word.split()))
+    print(df.head())
